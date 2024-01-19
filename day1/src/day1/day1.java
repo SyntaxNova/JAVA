773092a518e0fd678a700ae93cb57ba9115cab5c
“Atharva <atharvacodes@gmail.com>
2024-01-19 20:29:40 +0530

    configuration

--- day1/src/day1/day1.java
+++ day1/src/day1/day1.java
@@ -1,13 +1,43 @@
 package day1;
-
+import java.util.*;
 public class day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello world\n");
-		//also use println insted of \n
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		/*System.out.print("Hello world\n");
+		//also use println instead of \n
 		System.out.print("Hello world \n");
 		System.out.print("*\n**\n***\n****");
+		*/
+		/*primitive
+		  byte
+		  short
+		  char
+		  boolean
+		  int 
+		  long
+		  float
+		  double
+		 */
+		/*
+		 NON-PRIMITIVE
+		 string
+		 array
+		 class
+		 object
+		 interface
+		 */
+		//conditional statements
+		int age = sc.nextInt();
+		
+		if(age>18) {
+			System.out.println("Can vote ");
+		}else {
+			System.out.println("Cannot vote ");
+		}
 	}
 
 }
---
+++ file_config/src/file_config/file_config.java
@@ -0,0 +1,11 @@
+package file_config;
+
+public class file_config {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! ");
+		
+	}
+
+}
