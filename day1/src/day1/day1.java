90a5bd22d18b54d99adb9ae381c260d57e92520f
“Atharva <atharvacodes@gmail.com>
2024-01-19 16:11:38 +0530

    commit

---
+++ day1/src/day1/day1.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world\n");
+		//also use println insted of \n
+		System.out.print("Hello world \n");
+		System.out.print("*\n**\n***\n****");
+	}
+
+}
